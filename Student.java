0cce7ef466f6136abf351426c7cfaf66c4cd5889
Nenavath Anusha <anushanenavath2002@gmail.com>
2026-05-16 10:47:06 +0530

    Initial commit

--- Student.java
+++ Student.java
@@ -1,5 +1,5 @@
 public class Student {
-    public static void main (String[]args){
-    System.out.println("hello world");
+    public static void main(String[]args){
+        System.out.println("Hello from Test class");
     }
 }
